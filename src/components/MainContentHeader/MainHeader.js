bd4590399c0b4285148f8c3fdf8a6465feafa3d0
Ruslan <ruslanworkconsult@gmail.com>
2020-01-10 17:09:41 +0200

    jnet: * (bootstrap) grit was fixed

--- src/components/MainContentHeader/MainHeader.js
+++ src/components/MainContentHeader/MainHeader.js
@@ -1,17 +1,20 @@
 import React from "react"
 import UserInfo from "./UserInfo"
 import LeftSideBlock from "../LeftSide/LeftSideBlock"
+import Container from "../Main/Container"
 
 const MainContentHeader = () => (
   <div className="container-fluid">
     <div className="row">
-      <div className="col-3 col-sm-3 col-md-3 col-lg-2 px-2 px-sm-3">
-        <div className="left-side-container">
+      <div className="col-3 col-sm-3 col-md-3 col-lg-2 px-2 px-sm-3 px-xl-4 d-flex justify-content-center">
+        <div className="left-side-container w-100">
           <LeftSideBlock />
         </div>
       </div>
-      <div className="col-9 col-sm-9 col-md-9 col-lg-10 px-2 px-sm-3 main-header-right-col">
-        <UserInfo />
+      <div className="col-9 col-sm-9 col-md-9 col-lg-10 px-2 px-sm-3 px-xl-4 main-header-right-col">
+        <Container>
+          <UserInfo />
+        </Container>
       </div>
     </div>
   </div>
--- src/components/index.js
+++ src/components/index.js
@@ -13,8 +13,8 @@ import LeftSideBlock from "./LeftSide/LeftSideBlock"
 const MainContent = () => (
   <div className="container-fluid">
     <div className="row">
-      <div className="col-3 col-sm-3 col-md-3 col-lg-2 px-2 px-sm-3">
-        <div className="left-side-container py-4 h-100">
+      <div className="col-3 col-sm-3 col-md-3 col-lg-2 px-2 px-sm-3 px-xl-4 d-flex justify-content-center">
+        <div className="left-side-container py-4 h-100 w-100">
           <LeftSideBlock img={house} text="Startseite" />
           <LeftSideBlock img={news} text="Nachrichten" />
           <LeftSideBlock img={jobagent} text="Jobagent" />
@@ -24,7 +24,7 @@ const MainContent = () => (
           <LeftSideBlock img={support} text="Support" />
         </div>
       </div>
-      <div className="col-9 col-sm-9 col-md-9 col-lg-10 px-2 px-sm-3">
+      <div className="col-9 col-sm-9 col-md-9 col-lg-10 px-2 px-sm-3 px-xl-4">
         <Main />
       </div>
     </div>
