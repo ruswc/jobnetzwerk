d42e288a2b6dd5c5040ccea21ef8527d54fa99ab
Ruslan <ruslanworkconsult@gmail.com>
2020-01-27 11:00:13 +0200

    jnet: * (notfoundpage) apostrophe appearence was fixed

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -12,7 +12,7 @@ const Error = () => (
         <div className="notfound">
           <div className="notfound-404">
             <h1>Oops!</h1>
-            <h2>404 - The Page can{`&apos;`}t be found</h2>
+            <h2>404 - The Page can&apos;t be found</h2>
           </div>
           <Link to="/"> Go to Homepage </Link>
         </div>
