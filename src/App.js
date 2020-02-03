0aac82ba71d1dde4a006a6b8647932c27c679349
Ruslan <ruslanworkconsult@gmail.com>
2020-02-03 17:59:54 +0200

    jnet: * (pages) misspellings have been fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import First from './pages/First'
@@ -8,23 +7,21 @@ import Error from './pages/Error'
 
 function App() {
   return (
-    <>
-      <HashRouter basename={`${process.env.PUBLIC_URL}/`}>
-        <Switch>
-          <Route exact path="/" component={First} />
-          <Route exact path="/messagecenter">
-            <MessageCenter list />
-          </Route>
-          <Route exact path="/messagecenter/read">
-            <MessageCenter read />
-          </Route>
-          <Route exact path="/messagecenter/answer">
-            <MessageCenter answer />
-          </Route>
-          <Route path="/*" component={Error} />
-        </Switch>
-      </HashRouter>
-    </>
+    <HashRouter basename={`${process.env.PUBLIC_URL}/`}>
+      <Switch>
+        <Route exact path="/" component={First} />
+        <Route exact path="/messagecenter">
+          <MessageCenter list />
+        </Route>
+        <Route exact path="/messagecenter/read">
+          <MessageCenter read />
+        </Route>
+        <Route exact path="/messagecenter/answer">
+          <MessageCenter answer />
+        </Route>
+        <Route path="/*" component={Error} />
+      </Switch>
+    </HashRouter>
   )
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,12 @@
-// document.addEventListener("readystatechange", event => {
-// if (event.target.readyState === "interactive") {
-// same as:  ..addEventListener("DOMContentLoaded".. and   jQuery.ready
-// console.log("All HTML DOM elements are accessible");
-// }
+/*
+document.addEventListener('readystatechange', event => {
+  if (event.target.readyState === 'interactive') {
+    // same as:  ..addEventListener("DOMContentLoaded".. and   jQuery.ready
+    console.log('All HTML DOM elements are accessible')
+  }
 
-// if (event.target.readyState === "complete") {
-//     console.log("Now external resources are loaded too, like css,src etc... ")
-//     const chat = document.getElementById("chat-container")
-//     console.dir(chat)
-//     console.log(chat)
-//     window.addEventListener("scroll", function (a) {
-//         console.log(a)
-//     })
-// }
-// })
+  if (event.target.readyState === 'complete') {
+    console.log('Now external resources are loaded too, like css,src etc... ')
+  }
+})
+*/
