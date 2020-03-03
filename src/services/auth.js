60b73de4c9bbe678bf4e39346cd683056cb1a9c5
Ruslan <ruslanworkconsult@gmail.com>
2020-03-03 15:04:42 +0200

    jnet: * (routing) An experiment to change the routing address

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,7 +3,7 @@ import client from '../helpers/api'
 export default {
   signIn: data =>
     client({
-      url: '/jobnetzwerk/#/jobnetzwerk/login',
+      url: '/login',
       method: 'POST',
       data
     })
